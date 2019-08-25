717ae9e973bc930b0a47f77aae7e50d3a72c803f
sblundy <el_gonzzo@hotmail.com>
2019-08-25 09:16:29 -0700

    Clean up parser code

--- src/main/kotlin/com/github/sblundy/elvish/lang/parserUtils.kt
+++ src/main/kotlin/com/github/sblundy/elvish/lang/parserUtils.kt
@@ -4,21 +4,10 @@ package com.github.sblundy.elvish.lang
 
 import com.github.sblundy.elvish.psi.ElvishTypes
 import com.intellij.lang.PsiBuilder
+import com.intellij.lang.parser.GeneratedParserUtilBase
 
-private val keywords = setOf(
-    ElvishTypes.KEYWORD_DEL,
-    ElvishTypes.KEYWORD_ELIF,
-    ElvishTypes.KEYWORD_ELSE,
-    ElvishTypes.KEYWORD_EXCEPT,
-    ElvishTypes.KEYWORD_FINALLY,
-    ElvishTypes.KEYWORD_FN,
-    ElvishTypes.KEYWORD_FOR,
-    ElvishTypes.KEYWORD_IF,
-    ElvishTypes.KEYWORD_TRY,
-    ElvishTypes.KEYWORD_WHILE)
-
-fun parseKeywordAsBareword(builder: PsiBuilder, level: Int): Boolean {
-    if (!(builder.tokenType in keywords)) return false
+fun parseKeywordAsBareword(builder: PsiBuilder, @Suppress("UNUSED_PARAMETER") level: Int): Boolean {
+    if (builder.tokenType !in ElvishParserDefinition.KEYWORDS) return false
     builder.remapCurrentToken(ElvishTypes.BAREWORD_CHAR)
-    return true
+    return GeneratedParserUtilBase.consumeToken(builder, ElvishTypes.BAREWORD_CHAR)
 }
