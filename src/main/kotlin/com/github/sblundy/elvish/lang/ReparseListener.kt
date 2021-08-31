66a7c5415c20db2b77fad0a85ed8bddd934801c9
sblundy <el_gonzzo@hotmail.com>
2021-08-30 20:44:37 -0700

    Fix ReparseListener to fire reparseFiles in EDT

--- src/main/kotlin/com/github/sblundy/elvish/lang/ReparseListener.kt
+++ src/main/kotlin/com/github/sblundy/elvish/lang/ReparseListener.kt
@@ -1,15 +1,18 @@
 package com.github.sblundy.elvish.lang
 
 import com.github.sblundy.elvish.ElvishLanguage
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.impl.PsiManagerEx
 
 class ReparseListener(private val project: Project): com.github.sblundy.elvish.settings.ElvishSettings.Listener {
     override fun languageVersionChanged() {
-        val files = PsiManagerEx.getInstanceEx(project).fileManager.allCachedFiles.filter {
-            it.language.`is`(ElvishLanguage.INSTANCE)
-        }.map { it.virtualFile }
-        PsiDocumentManager.getInstance(project).reparseFiles(files, true)
+        ApplicationManager.getApplication().invokeLater {
+            val files = PsiManagerEx.getInstanceEx(project).fileManager.allCachedFiles.filter {
+                it.language.`is`(ElvishLanguage.INSTANCE)
+            }.map { it.virtualFile }
+            PsiDocumentManager.getInstance(project).reparseFiles(files, true)
+        }
     }
 }
